ce9c2c9c2c4cdffc0d7fa6028d65bbe84958833d
Sergey Simenshteyn <simenshteyn@gmail.com>
2019-09-20 19:46:42 +0700

    BidManager class in testing

--- targetbandit.py
+++ targetbandit.py
@@ -318,19 +318,24 @@ class Ad(object):
         self.posterior_samples = posterior.rvs(samples)
 
 
-class Bids(object):
+class BidManager(object):
     """Bids class to store information and behaviour about bids."""
     def __init__(self, max_bid=-1,
                  min_bid=-1,
                  bid_points=[],
-                 history=[]):
+                 history=[],
+                 current_bid_index=-1,
+                 current_time=-1):
         self.max_bid = max_bid
         self.min_bid = min_bid
         self.bid_points = bid_points
         self.history = history
+        self.current_bid_index = current_bid_index
+        self.current_time = current_time
 
-    def generate(self):
-        """Generate bids from max, all numbers in penny"""
+
+    def start(self):
+        """Start working. Generate bids from max, all numbers in penny"""
         def make_points(max_bid, min_bid):
             bid_points = [0] * 9
             bid_points[0] = min_bid
@@ -343,22 +348,53 @@ class Bids(object):
             bid_points[5] = ((bid_points[6] - bid_points[4]) // 2) + bid_points[4]
             bid_points[7] = ((bid_points[8] - bid_points[6]) // 2) + bid_points[6]
             return bid_points
+
         if (self.max_bid >= 30_00) and (self.max_bid <= 1000_00):
             self.min_bid = 30_00
             self.bid_points = make_points(self.max_bid, self.min_bid)
-            return True
+            self.current_time = 0
+            self.current_bid_index = self.choose_bid()
+            return self.bid_points[self.current_bid_index]
         elif (self.max_bid >= 1_20) and (self.max_bid <= 20_00):
             self.min_bid = 1_20
             self.bid_points = make_points(self.max_bid, self.min_bid)
-            return True
+            self.current_time = 0
+            self.current_bid_index = self.choose_bid()
+            return self.bid_points[self.current_bid_index]
         else:
-            print("some input error")
+            print("Start error")
             return False
 
+    def update(self, ad_id, ad_impressions):
+        """Get statistics for current ad."""
+        snap = {"ad_id": ad_id, "bid_index": self.current_bid_index, "ad_impressions": ad_impressions}
+        self.history.append(snap)
+        self.history[self.current_time].update(self.count_reward())
+        self.current_time += 1
+        self.current_bid_index = self.choose_bid()
+        return self.bid_points[self.current_bid_index]
+
+    def count_reward(self):
+
+        def previous_impressions(ad_id):
+            for snap in reversed(range(self.current_bid_index)):
+                if snap["ad_id"] == ad_id:
+                    return snap["ad_impressions"]
+            return 0
+
+        delta_impressions = self.history[self.current_time]["ad_impressions"] -\
+                            previous_impressions(self.history[self.current_time]["ad_id"])
+
+        reward = float(delta_impressions / self.bid_points[self.current_bid_index])
+        return {"reward": reward}
 
 
 
-
+    def choose_bid(self):
+        """Not real algo still, just for testing"""
+        import random
+        random_choice = random.choice([0,1,2,3,4,5,6,7,8])
+        return random_choice
 
 
 class Cabinet(object):
